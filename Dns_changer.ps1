c3680be4add47ca8682c7ba6af9c8593c4851446
Thomas <thomassouchet22@gmail.com>
2022-05-23 19:18:40 +0200

    Bugfix

--- Dns_changer.ps1
+++ Dns_changer.ps1
@@ -9,6 +9,7 @@ PowerShell.exe -windowstyle hidden {
 #IP
 $DnsIpPrimary = "8.8.8.8" #Your preferred DNS
 $DnsIpSecondary = "8.8.4.4" #Your auxiliary DNS
+$DnsDefault = "192.168.1.1" #Your default DNS
 
 #Interface Alias
 $InterfaceAlias = "Ethernet" #Your DNS interface to modify 
@@ -23,13 +24,15 @@ $DnsServerAddress = $DnsServer.Address
 $NewDnsServerAdressPrimary = [string]
 $NewDnsServerAdressSecondary = [string]
 
-If($DnsServerAddress -eq $DnsIpPrimary)
+If($DnsServerAddress -eq $DnsDefault)
 {
- $NewDnsServerAdressPrimary = "Automatic (DHCP)"
- $NewDnsServerAdressSecondary = "" 
-}else{
  $NewDnsServerAdressPrimary = $DnsIpPrimary
  $NewDnsServerAdressSecondary = $DnsIpSecondary
+ $default = $false
+}else{
+ $NewDnsServerAdressPrimary = $DnsDefault + "(Default)"
+ $NewDnsServerAdressSecondary = ""
+ $default = $true
 }
 
 #Loading the library
@@ -41,15 +44,17 @@ Configuration to be applied: $NewDnsServerAdressPrimary ; $NewDnsServerAdressSec
 Click on [no] to change the configuration", 4131,"DNS-changer")
 
 #Information window
-function Info-Confirm ($InfoText) {
+function Info-Confirm ([string]$InfoText) {
     [Microsoft.VisualBasic.Interaction]::MsgBox("$InfoText", 4160,"DNS-changer")
 }
 
 #Change DNS function
 # Param :
-# $DnsPrimary and $DnsSecondary => New DNS server addresses
-function change-Dns ($DnsPrimary, $DnsSecondary) {
-    If($DnsPrimary -eq "Automatic (DHCP)")
+# $DnsPrimary and $DnsSecondary => [string]: New DNS server addresses
+# $isDefault => [bool] is the default configuration to be applied
+function change-Dns ([string]$DnsPrimary, [string]$DnsSecondary, [bool]$isDefault) {
+    echo $DnsPrimary
+    If($isDefault)
     {
         #Reset default configuration
         Set-DnsClientServerAddress -InterfaceAlias $InterfaceAlias -ResetServerAddresses
@@ -68,13 +73,13 @@ function change-Dns ($DnsPrimary, $DnsSecondary) {
 If($Result -eq 6) 
 { 
     #If yes  
-    $change = change-Dns $NewDnsServerAdressPrimary, $NewDnsServerAdressSecondary
+    $change = change-Dns $NewDnsServerAdressPrimary $NewDnsServerAdressSecondary $default
 }elseif ($Result -eq 7) {
     #If no
     #Input new Dns server adress primary and secondary
     $NewDnsServerAdressPrimary = [Microsoft.VisualBasic.Interaction]::InputBox('Enter your preferred DNS','DNS-changer', $DnsIpPrimary)
     $NewDnsServerAdressSecondary = [Microsoft.VisualBasic.Interaction]::InputBox('Enter your auxiliary DNS','DNS-changer', $DnsIpSecondary)
-    $change = change-Dns $NewDnsServerAdressPrimary, $NewDnsServerAdressSecondary
+    $change = change-Dns $NewDnsServerAdressPrimary $NewDnsServerAdressSecondary $false
     
 }else{
     #Cancel
